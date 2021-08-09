47c061a8fdb84eab2075349799fe6c5269fe1087
Daniel Gekeler <181665651+DanielGekeler@users.noreply.github.com>
2021-08-09 12:23:18 +0200

    moved functions for loading of world data from main.go to loader.go

--- src/main.go
+++ src/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
-	"math"
 	"os"
 
 	"github.com/Tnze/go-mc/save"
@@ -52,40 +51,6 @@ type chunk_meta struct {
 	compression int
 }
 
-func parse_chunks_from_region(region []byte) []chunk_meta {
-	locations := split_bytes(region[:4096], 4)
-	time_data := split_bytes(region[4096:8192], 4)
-
-	ret := make([]chunk_meta, 1024)
-
-	for i := 0; i < len(ret); i++ {
-		pos := append([]byte{0}, locations[i][:3]...)
-		sector_length := locations[i][3]
-
-		offset := bytes_to_int(pos)
-		chunk_time := bytes_to_int(time_data[i])
-
-		x, z := calculate_chunk_pos(i)
-
-		length := bytes_to_int(region[offset*4096 : offset*4096+4])
-		compression := int(region[offset*4096+4])
-
-		ret[i] = chunk_meta{offset: offset,
-			sectors: int(sector_length),
-			time:    chunk_time,
-			x:       x, z: z,
-			length:      length,
-			compression: compression,
-		}
-	}
-
-	return ret
-}
-
-func calculate_chunk_pos(index int) (int, int) {
-	return index % 32, index / 32
-}
-
 // decompress_chunk takes the specific part of the region and returns a []byte of the raw NBT data
 /*func decompress_chunk(chunk chunk_meta, region []byte) []byte {
 	a := (chunk.offset * 4096) + 5
@@ -97,34 +62,6 @@ func calculate_chunk_pos(index int) (int, int) {
 	r.Close()
 	return raw_nbt
 }*/
-
-// Calculate how many bits are needed to index the elements in the pallete of a chunk section
-func index_bit_length(palette []save.Block) int {
-	bits := 4
-	for math.Pow(2.0, float64(bits)) < float64(len(palette)) {
-		bits++
-	}
-	return bits
-}
-
-// Parse namespaced block IDs from nbt data
-// Block IDs in each section are asigned an index by the pallete
-// multiple indexes are stored in one int64
-// bit_length is the number of bits needed for an index number in the pallete
-// this function returns a []string of the namespaced block IDs
-func nbt_to_block(long int64, pallete []save.Block, bit_length int) (block_id []string) {
-	// mask works like a subnetmask to get the last (bit_length) bits
-	mask := byte(math.Pow(2.0, float64(bit_length)) - 1)
-
-	// shift the input number by a diffrent amount
-	// with each iteration to get all indexes
-	for i := 0; i < 64/bit_length; i++ {
-		shifted := long >> (i * bit_length) // shifting
-		block := shifted & int64(mask)      // get only the last (bit_length) bits
-		block_id = append(block_id, pallete[block].Name)
-	}
-	return
-}
 
 // Get the top most blocks (visible from the top)
 // returns a slice of the namespaced block IDs
@@ -164,18 +101,4 @@ func top_subchunk(sections []save.Chunk) int {
 		}
 	}
 	return 0
-}
-
-func load_chunk(chunk chunk_meta, region []byte) save.Column {
-	// calculate offsets
-	a := (chunk.offset * 4096) + 4
-	b := chunk.length
-
-	data := region[a : a+b] // the raw bytes of the chunk data
-
-	var c save.Column // Column means the whole chunk (0-255)...
-	if err := c.Load(data); err != nil {
-		panic(err)
-	}
-	return c
 }
