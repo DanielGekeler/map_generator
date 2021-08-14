1e0a1e92949e454741e04ef59c18e785495c3bbc
Daniel Gekeler <181665651+DanielGekeler@users.noreply.github.com>
2021-08-14 16:58:53 +0200

    added render_chunk() to read all blocks in a chunk and output the pixel data to channel

--- src/render.go
+++ src/render.go
@@ -27,3 +27,27 @@ func draw_map(chn chan mappixel, filename string, x, z int) {
 	png.Encode(f, img)
 	f.Close()
 }
+
+func render_chunk(chunk chunk_meta, region []byte, chn chan mappixel, min, max pos2d) {
+	c := load_chunk(chunk, region)
+	vis := visible_blocks(c)
+
+	for xi, x := range vis {
+		stopx := xi + (chunk.x * 16)
+		if stopx < min.X || stopx > max.X {
+			continue
+		}
+		for zi, z := range x {
+			stopz := zi + (chunk.z * 16)
+			if stopz < min.Z || stopz > max.Z {
+				continue
+			}
+
+			color := color_id[z]
+
+			a := xi + (16 * chunk.x) - min.X
+			b := zi + (16 * chunk.z) - min.Z
+			chn <- mappixel{a, b, color}
+		}
+	}
+}
